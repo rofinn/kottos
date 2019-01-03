fb5b150d5f7a5353fccaabb6c170f7cac344f526
Rory Finnegan <rory.finnegan@gmail.com>
2019-01-03 00:10:28 -0600

    Got tests working again and properly reading/writing to registers/addresses now.

---
+++ kottos/modbus/client.py
@@ -0,0 +1,61 @@
+import socket
+
+
+class Client:
+    """
+    A modbus client for managing connections and provides a more human 
+    readable interface for reading state from a server based on the 
+    specified register table.
+    """
+    def __init__(self, host, port, table):
+        self._host = host
+        self._port = port
+        self._table = table
+    
+    @property
+    def host(self):
+        return self._host
+    
+    @property
+    def port(self):
+        return self._port
+    
+    @property
+    def table(self):
+        return self._table
+    
+    def scan(self, slave_id=0):
+        """
+        Reads raw values from the modbus server and converts the values to an appropriate format.
+        """
+        vals = self.read(slave_id)
+        return self.convert(vals)
+
+    def read(self, slave_id=0):
+        """
+        Reads the raw values from the registers specified by the register table.
+        Returns a dict with 
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        resp = {}
+
+        try:
+            for reg in self.table:
+                # Read the raw data from the register
+                resp[reg.label] = reg.read(sock, slave_id)
+        finally:
+            sock.close()
+
+        return resp
+    
+    def convert(self, vals):
+        """
+        Converts the raw values passed in to the appropriate format.
+        """
+        resp = {}
+
+        for reg in self.table:
+            resp[reg.label] = reg.convert(vals[reg.label])
+        
+        return resp
--- tests/test_modbus.py
+++ tests/test_modbus.py
@@ -1,56 +1,26 @@
 import logging
-import struct
+import six
 import threading
 
-from umodbus import conf
-from umodbus.server.tcp import RequestHandler, get_server
-from umodbus.utils import log_to_stream
-from socketserver import TCPServer
-
-from kottos.modbus import CC_REGISTER_TABLE, Client
-
-# Add stream handler to logger 'uModbus'.
-log_to_stream(level=logging.DEBUG)
-
-# Extract the addresses from our default dict
-addresses = [r[0] for r in CC_REGISTER_TABLE.values()]
-
-# A very simple data store which maps addresses against their values.
-data_store = dict(zip(addresses, range(0, len(addresses))))
-
-# Enable values to be signed (default is False).
-conf.SIGNED_VALUES = True
-
-TCPServer.allow_reuse_address = True
-app = get_server(TCPServer, ('localhost', 54321), RequestHandler)
-
-@app.route(slave_ids=[0], function_codes=[3], addresses=addresses)
-def read_data_store(slave_id, function_code, address):
-    """" Return value of address. """
-    return data_store[address]
-
-
-def modbus_server():
-    try:
-        app.serve_forever()
-    finally:
-        app.shutdown()
-        app.server_close()
+import kottos.modbus
+from kottos.modbus.registers import MNS_REGISTER_TABLE
+from kottos.modbus.client import Client
+from kottos.modbus.server import serve, data_store
 
 
 def test_read():
     for k, v in data_store.items():
         print('{}: {}'.format(k, v))
 
-    t = threading.Thread(target=modbus_server, daemon=True)
+    t = threading.Thread(target=serve, daemon=True)
     t.start()
-    c = Client('localhost', 54321, CC_REGISTER_TABLE)
+    c = Client('localhost', 54321, MNS_REGISTER_TABLE)
     states = c.read(0)
 
-    for k, v in CC_REGISTER_TABLE.items():
-        print('{}: {}'.format(k, v))
-        assert v[1](data_store[v[0]]) == states[k]
-
-
-if __name__ == '__main__':
-    modbus_server()
+    for r in MNS_REGISTER_TABLE:
+        print(
+            '{} ({}): {}, {}'.format(
+                r.label, r.register, states[r.label], data_store[r.register - 1]
+            )
+        )
+        assert data_store[r.register - 1] == states[r.label]
